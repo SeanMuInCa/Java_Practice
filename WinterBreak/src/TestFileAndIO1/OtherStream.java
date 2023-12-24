9d3ae1ff626aef97d7edd19a9b52a309e335ae3e
Sean <fengying1028@hotmail.com>
2023-12-24 16:31:14 -0600

    io practice

---
+++ WinterBreak/src/TestFileAndIO1/OtherStream.java
@@ -0,0 +1,55 @@
+package TestFileAndIO1;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLOutput;
+
+/**
+ * Purpose:             TestFileAndIO1<br />
+ * Data Submitted:      2023/12/24 <br />
+ * Assignment Number:    TestFileAndIO1<br />
+ * Course Name:         COSC601  <br />
+ * Instructor:          George Ding  <br />
+ * File Path:          TestFileAndIO1 <br />
+ *
+ * @author Zhenghua Mu
+ * @version 1.0.0
+ * 输入一段文字，输出大写形式
+ */
+public class OtherStream
+{
+    @Test
+    public void test(){
+        System.out.println("input a string(e for exit): ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = null;
+        try
+        {
+            while ((str = br.readLine())!= null)
+            {
+                if (str.equals("e"))
+                {
+                    break;
+                }
+                System.out.println("--> " + str.toUpperCase());
+                System.out.println("input a string ");
+            }
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }finally
+        {
+            try
+            {
+                if(br != null)
+                    br.close();
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+}
