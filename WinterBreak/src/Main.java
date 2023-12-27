8cfec79deb2ac652ab13dbda3ad1d078657c6162
Sean <fengying1028@hotmail.com>
2023-12-26 21:35:40 -0600

    io practice

--- WinterBreak/src/Main.java
+++ WinterBreak/src/Main.java
@@ -13,6 +13,33 @@ public class Main
 {
     public static void main(String[] args)
     {
+        extracted();
         System.out.println("Hello world!");
+
+        hanoi(3, 'A', 'B', 'C');
     }
+
+    public static void hanoi(int n, char A, char B, char C)
+    {
+        if (n == 1)
+        {
+            System.out.println(n + " from " + A +" ----> " + C);
+        } else
+        {
+            hanoi(n - 1, A, C, B);
+            System.out.println(n + " from " + A +" ----> " + C);
+            hanoi(n - 1, B, A, C);
+        }
+    }
+
+    private static void extracted()
+    {
+        int m = 10;
+        int n = 20;
+        int temp = m;
+        m = n;
+        n = temp;
+        System.out.println(m + " " + n);
+    }
+
 }
