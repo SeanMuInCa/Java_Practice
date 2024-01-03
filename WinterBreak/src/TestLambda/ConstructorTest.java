01b96a9cf6b3f1ba2d39196f64afa75042b702e3
Sean <fengying1028@hotmail.com>
2024-01-02 18:48:47 -0600

    lambda and method reference

---
+++ WinterBreak/src/TestLambda/ConstructorTest.java
@@ -0,0 +1,23 @@
+package TestLambda;
+
+import org.junit.Test;
+
+/**
+ * Purpose:             TestLambda<br />
+ * Data Submitted:      2024/1/2 <br />
+ * Assignment Number:    TestLambda<br />
+ * Course Name:         COSC601  <br />
+ * Instructor:          George Ding  <br />
+ * File Path:          TestLambda <br />
+ *
+ * @author Zhenghua Mu
+ * @version 1.0.0
+ * 构造器引用
+ */
+public class ConstructorTest
+{
+    @Test
+    public void test(){
+
+    }
+}
