ee09e6003e89679ef30eebaa271388cb0a41be7c
Sean <fengying1028@hotmail.com>
2024-01-04 10:13:43 -0600

    stream practice

--- WinterBreak/src/TestStream/TestStream1.java
+++ WinterBreak/src/TestStream/TestStream1.java
@@ -2,7 +2,9 @@ package TestStream;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
@@ -22,17 +24,22 @@ public class TestStream1
     @Test
     public void test1(){
         List<Person> list = PersonData.getPersonList();
-        Stream<Person> stream = list.stream();
-        System.out.println(stream);
+        Stream<Person> stream = list.stream();//顺序流
+
+        Stream<Person> personStream = list.parallelStream();//并行流
     }
     //创建stream方式2，通过数组
     @Test
     public void test2(){
+        Integer[] arr = new Integer[]{1,2,3,4,5};
+        Stream<Integer> stream = Arrays.stream(arr);
 
+        int[] arr1 = new int[]{1,2,3,4,5};
+        IntStream stream1 = Arrays.stream(arr1);//IntStream
     }
     //创建stream方式3，通过Stream 的of()方法
     @Test
     public void test3(){
-
+        Stream<String> stringStream = Stream.of("a", "b", "c");
     }
 }
