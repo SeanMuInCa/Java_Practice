46e9b171087f846bdf6e03572d585c39de8c9145
Sean <fengying1028@hotmail.com>
2024-01-04 19:32:17 -0600

    stream practice

--- WinterBreak/src/TestStream/TestStream2.java
+++ WinterBreak/src/TestStream/TestStream2.java
@@ -3,6 +3,8 @@ package TestStream;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -52,6 +54,21 @@ public class TestStream2
         list.stream().map(String::toUpperCase).forEach(System.out::println);
 
         List<Person> list1 = PersonData.getPersonList();
+        list1.stream().filter(person -> person.getName().startsWith("raina")).forEach(System.out::println);
         list1.stream().filter(person -> person.getName().startsWith("raina")).map(Person::getName).forEach(System.out::println);
+    }
+    //排序
+    @Test
+    public void test3(){
+        Integer[] arr = new Integer[]{88,23,12,34,56,78};//stream不会改变这个原数组
+        String[] arr1 = new String[]{"bb","dd","aa","cc","ee","ff"};
+
+        Arrays.stream(arr).sorted().forEach(System.out::println);
+
+        Comparator<Integer> com = (i1, i2) -> i2 - i1;
+        Arrays.stream(arr).sorted(com).forEach(System.out::println);
+        Comparator<String> comparator = (o1, o2) -> -o1.compareTo(o2);
+//        Comparator<String> comparator = String::compareTo;
+        Arrays.stream(arr1).sorted(comparator).forEach(System.out::println);
     }
 }
