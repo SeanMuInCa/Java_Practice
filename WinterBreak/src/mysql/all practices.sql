c10c9f1760643de3c77a5496ef554eca651cf10a
Sean <fengying1028@hotmail.com>
2024-01-08 10:44:48 -0600

    sub query

--- WinterBreak/src/mysql/all practices.sql
+++ WinterBreak/src/mysql/all practices.sql
@@ -606,3 +606,29 @@ FROM employees JOIN departments
 ON employees.department_id = departments.department_id
 WHERE departments.location_id = 1700
 
+#6. 查询管理者是King的员工姓名和工资
+SELECT last_name,salary
+FROM employees
+WHERE manager_id IN (
+										SELECT employee_id
+										FROM employees
+										WHERE last_name = 'King'
+										)
+
+#7. 查询工资最低的员工信息 (last_name, salary)
+SELECT last_name,salary
+FROM employees
+WHERE salary = (
+								SELECT MIN(salary)
+								FROM employees
+								)
+								
+#8. 查询平均工资最低的部门信息
+SELECT department_id,AVG(salary)
+FROM employees
+GROUP BY department_id
+HAVING AVG(salary) <= ALL(
+													SELECT AVG(salary)
+													FROM employees
+													GROUP BY department_id 
+													)
