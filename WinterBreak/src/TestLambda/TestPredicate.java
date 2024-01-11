debf64bb3c52fa644a4c6e3754a99c2f102fd099
Sean <fengying1028@hotmail.com>
2024-01-10 21:46:34 -0600

    LambdaTest

---
+++ WinterBreak/src/TestLambda/TestPredicate.java
@@ -0,0 +1,43 @@
+package TestLambda;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+/**
+ * Purpose:             TestLambda<br />
+ * Data Submitted:      2024/1/10 <br />
+ * Assignment Number:    TestLambda<br />
+ * Course Name:         COSC602  <br />
+ * Instructor:          George Ding  <br />
+ * File Path:          TestLambda <br />
+ *
+ * @author Zhenghua Mu
+ * @version 1.0.0
+ */
+public class TestPredicate
+{
+    @Test
+    public void test1(){
+        ArrayList<String> list = new ArrayList<>();
+        list.add("hello");
+        list.add("java");
+        list.add("polo");
+        list.add("ok");
+
+        // forEach
+        list.forEach(System.out::println);
+
+        // removeIf
+        list.removeIf(s -> containsIgnoreCase(s, "o"));
+
+        System.out.println("---------------");
+        // forEach
+        list.forEach(System.out::println);
+    }
+
+    private boolean containsIgnoreCase(String str, String target)
+    {
+        return str.toLowerCase().contains(target.toLowerCase());
+    }
+}
