d8a4466a2c99dcbee13608b094f2ac3416bc4384
Sean <fengying1028@hotmail.com>
2024-01-10 21:02:34 -0600

    LambdaTest

---
+++ WinterBreak/src/TestLambda/Test1.java
@@ -0,0 +1,37 @@
+package TestLambda;
+
+/**
+ * Purpose:             TestLambda<br />
+ * Data Submitted:      2024/1/10 <br />
+ * Assignment Number:    TestLambda<br />
+ * Course Name:         COSC602  <br />
+ * Instructor:          George Ding  <br />
+ * File Path:          TestLambda <br />
+ *
+ * @author Zhenghua Mu
+ * @version 1.0.0
+ */
+public class Test1
+{
+    public static void main(String[] args)
+    {
+        //void 无参数形式函数接口修改Lambda表达式
+        callSomething(new Call()
+        {
+            @Override
+            public void shout()
+            {
+                System.out.println("hello,world");
+            }
+        });
+        callSomething(() -> System.out.println("hello"));
+        callSomething(() -> System.out.println("world"));
+    }
+    public static void callSomething(Call call){
+        call.shout();
+    }
+}
+interface Call
+{
+    void shout();
+}
