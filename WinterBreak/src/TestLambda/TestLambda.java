429cfff27a8bc0a96ab458aca65ff87ddbc25bb1
Sean <fengying1028@hotmail.com>
2024-01-11 22:46:54 -0600

    LambdaTest

--- WinterBreak/src/TestLambda/TestLambda.java
+++ WinterBreak/src/TestLambda/TestLambda.java
@@ -2,10 +2,7 @@ package TestLambda;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -115,5 +112,15 @@ public class TestLambda
         List<Integer> numbers = Arrays.asList(9, 10, 3, 4, 7, 3, 4);
         List<Integer> distinct = numbers.stream().distinct().collect(Collectors.toList());
         System.out.printf("Original List : %s,  Square Without duplicates : %s %n", numbers, distinct);
+    }
+    @Test
+    public void test6(){
+        //获取数字的个数、最小值、最大值、总和以及平均值
+        List<Integer> primes = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
+        IntSummaryStatistics stats = primes.stream().mapToInt((x) -> x).summaryStatistics();
+        System.out.println("Highest prime number in List : " + stats.getMax());
+        System.out.println("Lowest prime number in List : " + stats.getMin());
+        System.out.println("Sum of all prime numbers : " + stats.getSum());
+        System.out.println("Average of all prime numbers : " + stats.getAverage());
     }
 }
