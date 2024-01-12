364c99be479e01e280da47f1c524fcd7404eb097
Sean <fengying1028@hotmail.com>
2024-01-11 22:12:51 -0600

    LambdaTest

--- WinterBreak/src/TestLambda/TestLambda.java
+++ WinterBreak/src/TestLambda/TestLambda.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 /**
  * Purpose:             TestLambda<br />
@@ -54,5 +55,18 @@ public class TestLambda
         features.forEach(str -> System.out.println(str));
         System.out.println("-----------------------");
         features.forEach(System.out::println);
+    }
+    @Test
+    public void test3(){
+        List<String> languages = Arrays.asList("Java", "Scala", "C++", "Haskell", "Lisp");
+
+        filter(languages,str -> ((String)str).startsWith("J"));
+    }
+    public static void filter(List<String> names, Predicate condition) {
+        for(String  name: names)  {
+            if(condition.test(name)) {
+                System.out.println(name + " ");
+            }
+        }
     }
 }
