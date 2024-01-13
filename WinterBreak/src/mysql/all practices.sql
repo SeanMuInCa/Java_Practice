8e5f6962cb61a569b60dbd2936cf5dd570d2aba5
Sean <fengying1028@hotmail.com>
2024-01-13 11:39:57 -0600

    create table

--- WinterBreak/src/mysql/all practices.sql
+++ WinterBreak/src/mysql/all practices.sql
@@ -1108,7 +1108,7 @@ sex VARCHAR(5),
 CONSTRAINT FOREIGN KEY (officeCode) REFERENCES offices(officeCode)
 )
 
-desc employees
+
 
 #3. 将表employees的mobile字段修改到officeCode字段后面
 ALTER TABLE employees
@@ -1118,3 +1118,19 @@ MODIFY  mobile VARCHAR(25) AFTER officeCode
 ALTER TABLE employees
 CHANGE birth employee_birth DATETIME 
 
+#5. 修改sex字段，数据类型为CHAR(1)，非空约束
+ALTER TABLE employees
+MODIFY sex VARCHAR(1) NOT NULL
+
+#6. 删除字段note
+ALTER TABLE employees
+DROP COLUMN note
+
+#7. 增加字段名favoriate_activity，数据类型为VARCHAR(100)
+ALTER TABLE employees
+ADD favoriate_activity VARCHAR(100)
+
+#8. 将表employees名称修改为employees_info
+RENAME TABLE employees TO employees_info
+
+desc employees_info
