77c644d933871e991df54a7d4c52f6cc6fe4c069
Sean <fengying1028@hotmail.com>
2024-01-14 15:32:00 -0600

    recursive test

--- WinterBreak/src/TestRecursive/Textbook.java
+++ WinterBreak/src/TestRecursive/Textbook.java
@@ -2,6 +2,8 @@ package TestRecursive;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 /**
  * Purpose:             TestRecursive<br />
  * Data Submitted:      2024/1/14 <br />
@@ -18,7 +20,12 @@ public class Textbook
     @Test
     public void test1(){
         printMSG(5,"hello");
-        System.out.println(isPalindromes("uncle"));
+        System.out.println(isPalindromes("daaad"));
+        int[] arr = new int[]{4,2,2,5,15,11,62,4,6};
+        recSelectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+        System.out.println(recBinarySearch(arr, 11));
     }
     public void printMSG(int times, String msg){
         if(times == 1) System.out.println(msg);
@@ -28,8 +35,46 @@ public class Textbook
         }
     }
     public boolean isPalindromes(String str){
-        if(str.length() <= 1) return true;
-        else if(str.charAt(0) != str.charAt(str.length() - 1)) return false;
-        else return isPalindromes(str.substring(1,str.length() - 2));
+        if(str.length() <= 1) return true; //base case
+        else if(str.charAt(0) != str.charAt(str.length() - 1)) return false;//base case
+        else return isPalindromes(str.substring(1,str.length() - 1));
+    }
+    public void recSelectionSort(int[] arr){
+        recSelectionSort(arr,0,arr.length - 1);
+    }
+
+    private void recSelectionSort(int[] arr, int low, int high)
+    {
+        if(low < high){
+            //find the smallest number and index
+            int indexOfMin = low;
+            int min = arr[indexOfMin];
+            for (int i = low + 1; i <= high ; i++)
+            {
+                if(arr[i] < min){
+                    min = arr[i];
+                    indexOfMin = i;
+                }
+            }
+            //swap the smallest in arr with arr[low]
+            arr[indexOfMin] = arr[low];
+            arr[low] = min;
+            //sort the rest
+            recSelectionSort(arr,low + 1, high);
+        }
+    }
+    public int recBinarySearch(int[] arr, int key){
+        int left = 0;
+        int right = arr.length - 1;
+        return recBinarySearch(arr,key,left,right);
+    }
+
+    private int recBinarySearch(int[] arr, int key, int left, int right)
+    {
+        if(left > right) return - left - 1;
+        int mid = (left + right) / 2;
+        if(key < arr[mid]) return recBinarySearch(arr,key, 0, mid - 1);
+        else if (key == arr[mid]) return mid;
+        else return recBinarySearch(arr,key,mid + 1, right);
     }
 }
