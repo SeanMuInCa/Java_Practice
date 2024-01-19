7ba1ace7ddd24c379997d81c2109c83fc84c2c4c
Sean <fengying1028@hotmail.com>
2024-01-18 22:29:05 -0600

    linked list

---
+++ WinterBreak/src/MyLinkedList/SingleLinked.java
@@ -0,0 +1,82 @@
+package MyLinkedList;
+
+import java.util.Iterator;
+
+/**
+ * Purpose:             MyLinkedList<br />
+ * Data Submitted:      2024/1/18 <br />
+ * Assignment Number:    MyLinkedList<br />
+ * Course Name:         COSC602  <br />
+ * Instructor:          George Ding  <br />
+ * File Path:          MyLinkedList <br />
+ *
+ * @author Zhenghua Mu
+ * @version 1.0.0
+ */
+public class SingleLinked implements Iterable
+{
+    private Node first;
+    public void add(Object element)
+    {
+        Node newNode = new Node(element, null);
+        if(first == null){
+            first = newNode;
+            return;
+        }
+
+        Node node = first;
+        while(node.next != null){
+            node = node.next;
+        }
+        node.next = newNode;
+
+    }
+
+    @Override
+    public Itr iterator()
+    {
+        return new Itr();
+    }
+
+    private static class Node
+    {
+        Object data;
+        Node next;
+
+        public Node(Object data, Node next)
+        {
+            this.data = data;
+            this.next = next;
+        }
+    }
+    private class Itr implements Iterator
+    {
+        Node node = first;
+
+        @Override
+        public boolean hasNext()
+        {
+            return node != null;
+        }
+
+        @Override
+        public Object next()
+        {
+            Object data = node.data;
+            node = node.next;
+            return data;
+        }
+    }
+    public static void main(String[] args) {
+        //高内聚低耦合
+        SingleLinked link = new SingleLinked();
+        link.add("张三");
+        link.add("李四");
+        link.add("王五");
+        link.add("赵六");
+
+        for (Object o : link) {
+            System.out.println(o);
+        }
+    }
+}
