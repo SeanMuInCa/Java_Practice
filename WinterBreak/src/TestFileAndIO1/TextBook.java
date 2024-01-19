69ebe778b144c8d00dfede05e8c9456be11d85bd
Sean <fengying1028@hotmail.com>
2024-01-18 20:30:45 -0600

    textbook IO

--- WinterBreak/src/TestFileAndIO1/TextBook.java
+++ WinterBreak/src/TestFileAndIO1/TextBook.java
@@ -2,9 +2,7 @@ package TestFileAndIO1;
 
 import org.junit.Test;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.*;
 
 /**
  * Purpose:             TestFileAndIO1<br />
@@ -20,25 +18,80 @@ import java.util.Scanner;
 public class TextBook
 {
     @Test
-    public void test1(){
-        String str1, str2 = null;
+    public void test1()
+    {
+        String str1 = null, str2 = null;
         File sourceFile = new File("d:\\test\\a.txt");
-        try( Scanner input = new Scanner(sourceFile) )
+        try
         {
-           while (input.hasNext())
-           {
-               str1 = input.nextLine();
-               str2 = str1.replaceAll("abc","raina");
+            FileReader fr = new FileReader(sourceFile);
+            char[] buffer = new char[1024];
 
-           }
-            boolean delete = sourceFile.delete();
-            System.out.println(delete);
+            int length;
+            while ((length = fr.read(buffer)) != -1)
+            {//这里返回的是读取的长度
+                str1 += new String(buffer);
+            }
+            fr.close();
+            System.out.println(str1);
+            String s = str1.replaceAll("abc", "raina");
+            System.out.println(s);
+            FileWriter fw = new FileWriter(sourceFile);
+            fw.write(s);
+            fw.close();
            /*input.close();
            PrintWriter output = new PrintWriter(sourceFile);
            output.println(str2);*/
         } catch (FileNotFoundException e)
         {
             e.printStackTrace();
+        } catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void test2()
+    {
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+        try
+        {
+            fis = new FileInputStream("d:\\test\\a.txt");
+            int b = fis.read();
+            while (b != -1)
+            {
+                b = fis.read();
+            }
+        } catch (FileNotFoundException e)
+        {
+            throw new RuntimeException(e);
+        } catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        } finally
+        {
+            if (fis != null)
+            {
+                try
+                {
+                    fis.close();
+                } catch (IOException e)
+                {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (fos != null)
+            {
+                try
+                {
+                    fos.close();
+                } catch (IOException e)
+                {
+                    throw new RuntimeException(e);
+                }
+            }
         }
     }
 }
