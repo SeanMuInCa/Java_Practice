e82aa9d4b79d4dec3c8f64b48bf47c73d1341390
Sean <fengying1028@hotmail.com>
2024-01-23 10:54:53 -0600

    binary search tree

---
+++ WinterBreak/src/BinaryTree/Node.java
@@ -0,0 +1,20 @@
+package BinaryTree;
+
+/**
+ * Purpose:             BinaryTree<br />
+ * Data Submitted:      2024/1/23 <br />
+ * Assignment Number:    BinaryTree<br />
+ * Course Name:         COSC602  <br />
+ * Instructor:          George Ding  <br />
+ * File Path:          BinaryTree <br />
+ *
+ * @author Zhenghua Mu
+ * @version 1.0.0
+ */
+public class Node
+{
+    int data;
+    Node left;
+    Node right;
+
+}
