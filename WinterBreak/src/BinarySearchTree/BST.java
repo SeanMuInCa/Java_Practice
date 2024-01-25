d12fca8a834f1f17fa3fa773402d862bc19d2d9a
Sean <fengying1028@hotmail.com>
2024-01-24 20:51:06 -0600

    bst struct

--- WinterBreak/src/BinarySearchTree/BST.java
+++ WinterBreak/src/BinarySearchTree/BST.java
@@ -2,6 +2,7 @@ package BinarySearchTree;
 
 import BinaryTree.TreeNode;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 
 /**
@@ -134,6 +135,75 @@ public class BST<E> implements Tree<E>
     public TreeNode<E> getRoot()
     {
         return root;
+    }
+
+    public ArrayList<TreeNode<E>> path(E e)
+    {
+        ArrayList<TreeNode<E>> list = new ArrayList<>();
+        TreeNode<E> cur = root;
+
+        while (cur != null)
+        {
+            list.add(cur);
+            if(c.compare(e, cur.data) < 0)
+            {
+                cur = cur.left;
+            } else if (c.compare(e, cur.data) > 0)
+            {
+                cur = cur.right;
+            }else break;
+        }
+        return list;
+    }
+    @Override
+    public boolean delete(E e)
+    {
+        TreeNode<E> parent = null;
+        TreeNode<E> cur = root;
+        while (cur != null)
+        {
+            if (c.compare(e, cur.data) < 0)
+            {
+                parent = cur;
+                cur = cur.left;
+            } else if (c.compare(e, cur.data) > 0)
+            {
+                parent = cur;
+                cur = cur.right;
+            }else break;
+        }
+
+        if (cur == null) return false;
+
+        if (cur.left == null)
+        {
+            if (parent == null)
+            {
+                root = cur.right;
+            }else
+            {
+                if(c.compare(e, parent.data) < 0)
+                    parent.left = cur.right;
+                else parent.right = cur.right;
+            }
+        }else
+        {
+            TreeNode<E> parentOfRightMost = cur;
+            TreeNode<E> rightMost = cur.left;
+
+            while (rightMost.right != null)
+            {
+                parentOfRightMost = rightMost;
+                rightMost = rightMost.right;
+            }
+            cur.data = rightMost.data;
+
+            if(parentOfRightMost.right == rightMost)
+                parentOfRightMost.right = rightMost.left;
+            else parentOfRightMost.left = rightMost.left;
+        }
+        size--;
+        return true;
     }
     public static class TreeNode<E>
     {
