7eea7728bced7628c49a8979b3637971e075ce8c
Sean <fengying1028@hotmail.com>
2024-01-28 12:59:03 -0600

    huffman code

--- WinterBreak/src/BinarySearchTree/TestHeap.java
+++ WinterBreak/src/BinarySearchTree/TestHeap.java
@@ -20,7 +20,10 @@ public class TestHeap
         list.add(1);
         list.add(2);
         list.add(3);
-        list.add(2);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
         System.out.println(list);
         System.out.println(list.getSize());
     }
