98ed9d9e8213d1677b263fe90020f6923403fa3d
Sean <fengying1028@hotmail.com>
2024-02-01 11:10:43 -0600

    binarysearch for array

--- WinterBreak/src/Algrithm/Array/ArrayBinarySearch.java
+++ WinterBreak/src/Algrithm/Array/ArrayBinarySearch.java
@@ -13,6 +13,7 @@ package Algrithm.Array;
  */
 public class ArrayBinarySearch
 {
+    //左右边界都包含的情况下
     public int BinarySearch(int[] array, int target)
     {
         int left = 0;
@@ -23,6 +24,24 @@ public class ArrayBinarySearch
             if(array[mid] > target)
             {
                 right = mid - 1;
+            } else if (array[mid] < target)
+            {
+                left = mid + 1;
+            }else return mid;
+        }
+        return - left - 1;
+    }
+    //有边界不包含
+    public int BinarySearchR(int[] array, int target)
+    {
+        int left = 0;
+        int right = array.length - 1;
+        int mid = (left + right) / 2;
+        while (left < right)//这里是1小于还是小于等于，要看是否包含右边边界值
+        {
+            if(array[mid] > target)
+            {
+                right = mid;
             } else if (array[mid] < target)
             {
                 left = mid + 1;
