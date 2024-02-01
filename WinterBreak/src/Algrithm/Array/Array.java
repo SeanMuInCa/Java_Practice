724eaafad62fde03c6d916a2c741d396413763b6
Sean <fengying1028@hotmail.com>
2024-02-01 11:26:36 -0600

    delete

--- WinterBreak/src/Algrithm/Array/Array.java
+++ WinterBreak/src/Algrithm/Array/Array.java
@@ -1,5 +1,7 @@
 package Algrithm.Array;
 
+import java.util.Arrays;
+
 /**
  * Purpose:             Algrithm.Array<br />
  * Data Submitted:      2024/2/1 <br />
@@ -14,7 +16,7 @@ package Algrithm.Array;
 public class Array
 {
     //左右边界都包含的情况下
-    public int BinarySearch(int[] array, int target)
+    public static int BinarySearch(int[] array, int target)
     {
         int left = 0;
         int right = array.length - 1;
@@ -32,7 +34,7 @@ public class Array
         return - left - 1;
     }
     //有边界不包含
-    public int BinarySearchR(int[] array, int target)
+    public static int BinarySearchR(int[] array, int target)
     {
         int left = 0;
         int right = array.length;//不包含右
@@ -49,7 +51,7 @@ public class Array
         }
         return - left - 1;
     }
-    public int delete1(int[] array, int key)
+    public static int[] delete1(int[] array, int key)
     {
         int[] newArr = null;
         for (int i = 0; i < array.length; i++)
@@ -58,10 +60,19 @@ public class Array
             {
                 newArr = new int[array.length - 1];
                 System.arraycopy(array,0,newArr,0,i);
-                System.arraycopy(array,i+1,newArr,i+1,array.length - i);
+                System.arraycopy(array,i+1,newArr,i,array.length - i -1);
                 break;
             }
         }
-        return newArr == null ? 0 : newArr.length;
+        return newArr == null ? array : newArr;
+    }
+
+    public static void main(String[] args)
+    {
+        int[] arr = {1,2,3,4,5,6,7};
+        System.out.println(BinarySearch(arr, 4));
+        System.out.println(BinarySearchR(arr, 4));
+        int[] newArr = delete1(arr,0);
+        System.out.println(Arrays.toString(newArr));
     }
 }
