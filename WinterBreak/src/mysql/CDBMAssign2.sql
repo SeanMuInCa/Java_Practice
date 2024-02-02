6c8947884fc3435a940ffffecc90466154546471
Sean <fengying1028@hotmail.com>
2024-02-01 19:53:07 -0600

    assignment2 for database

--- WinterBreak/src/mysql/CDBMAssign2.sql
+++ WinterBreak/src/mysql/CDBMAssign2.sql
@@ -27,9 +27,16 @@ ORDER BY c.category_name ASC, p.product_name ASC;
 
 #5. Determine which products that have been ordered by more than 10 unique customers.
 
-SELECT o.product_id,c.customer_id  FROM customer_order c
-JOIN order_detail o on c.order_id = o.order_id
-GROUP BY o.product_id
+
+SELECT product_name FROM product
+WHERE product_id IN (
+		SELECT COUNT(*) res
+		FROM order_detail o
+		JOIN customer_order c ON o.order_id = c.order_id
+		GROUP BY product_id
+		HAVING res > 10
+);
+
 
 
 
@@ -39,14 +46,42 @@ GROUP BY o.product_id
 
 SELECT c.order_id,c.order_date,(quantity * price) subtotal
 FROM order_detail o JOIN product p ON o.product_id = p.product_id
-										JOIN customer_order c ON c.order_id = o.order_id
+										JOIN customer_order c ON c.order_id = o.order_id;
 
 
 
 #7. Select the names of all employees and their total dollar amount of sales (customer orders) they have placed. Order by the highest sales.
 
+
+SELECT e.first_name, e.last_name, result.res
+FROM employee e JOIN (
+	SELECT (o.quantity * p.price) res,employee_id FROM customer_order c
+	JOIN order_detail o ON c.order_id = o.order_id
+	JOIN product p ON p.product_id = o.product_id
+) result
+ORDER BY result.res DESC;
+
+
 #8. Select the names of all products that have been ordered at least once. You must use a subquery for this question.
+
+
+SELECT product_name FROM product
+WHERE product_id IN (
+	SELECT product_id FROM order_detail
+)
+
 
 #9. Select all the customer information and create a calculated a field called loyalty_program that categorizes customer with 10 or more orders as "gold", those with 5 to 9 as "silver", and everyone else with at least one order as "bronze". Customers with zero orders should have a NULL value.
 
+SELECT * 
+FROM customer JOIN (
+	SELECT COUNT(*) ordered
+	FROM customer_order
+	GROUP BY customer_id
+	) helper
+ON customer.customer_id = helper.customer_id
+
+
+
 # 10. Select the names of all customers who have placed orders that include a product with product_id 42. Use a correlated query.
+
