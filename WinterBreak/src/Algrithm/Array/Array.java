ea2a54ba43f4749a577d3a6cafe2a868d9397e9e
Sean <fengying1028@hotmail.com>
2024-02-02 10:53:09 -0600

    matrix

--- WinterBreak/src/Algrithm/Array/Array.java
+++ WinterBreak/src/Algrithm/Array/Array.java
@@ -107,6 +107,9 @@ public class Array
         return newArr;
     }
     //最小的连续子数组，注意是连续的，因此才会这么做
+    //Given an array of positive integers nums and a positive integer target, return the minimal length of a
+    //subarray
+    // whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
     public static int minSubArrayLen(int target, int[] nums)
     {
         int sum = 0;
@@ -118,10 +121,51 @@ public class Array
             while (sum >= target)
             {
                 res = Math.min(res,right - left + 1);
-                sum -= nums[left];
+                sum -= nums[left++];
             }
         }
         return res == Integer.MAX_VALUE ? 0 : res;
+    }
+    //给定一个正整数 n，生成一个包含 1 到 n^2 所有元素，且元素按顺时针顺序螺旋排列的正方形矩阵。
+    //
+    //示例:
+    //
+    //输入: 3 输出: [ [ 1, 2, 3 ], [ 8, 9, 4 ], [ 7, 6, 5 ] ]
+    public static int[][] generateMatrix(int n) {
+        int loop = 0;  // 控制循环次数
+        int[][] res = new int[n][n];
+        int start = 0;  // 每次循环的开始点(start, start)
+        int count = 1;  // 定义填充数字
+        int i, j;
+
+        while (loop++ < n / 2) { // 判断边界后，loop从1开始
+            // 模拟上侧从左到右
+            for (j = start; j < n - loop; j++) {
+                res[start][j] = count++;
+            }
+
+            // 模拟右侧从上到下
+            for (i = start; i < n - loop; i++) {
+                res[i][j] = count++;
+            }
+
+            // 模拟下侧从右到左
+            for (; j >= loop; j--) {
+                res[i][j] = count++;
+            }
+
+            // 模拟左侧从下到上
+            for (; i >= loop; i--) {
+                res[i][j] = count++;
+            }
+            start++;
+        }
+
+        if (n % 2 == 1) {
+            res[start][start] = count;
+        }
+
+        return res;
     }
     public static void main(String[] args)
     {
@@ -134,5 +178,7 @@ public class Array
         int[] newArr1 = afterSquare(arr);
         System.out.println(Arrays.toString(newArr1));
         System.out.println(minSubArrayLen(10,arr));
+        int[][] ints = generateMatrix(4);
+        System.out.println(Arrays.deepToString(ints));
     }
 }
