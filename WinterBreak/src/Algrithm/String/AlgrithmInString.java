126a7398062b1f491365530be427524b62352f7a
Sean <fengying1028@hotmail.com>
2024-02-05 23:04:12 -0600

    reverse

--- WinterBreak/src/Algrithm/String/AlgrithmInString.java
+++ WinterBreak/src/Algrithm/String/AlgrithmInString.java
@@ -39,14 +39,24 @@ public class AlgrithmInString
 
     public static String reverseK(String str, int k)
     {
-        for (int i = 0; i < str.length(); i += 2 * k)
+        char[] ch = str.toCharArray();
+        for (int i = 0; i < ch.length; i += 2 * k)
         {
-            if(i + k <= str.length())
+            int start = i;
+            // 判断尾数够不够k个来取决end指针的位置
+            int end = Math.min(ch.length - 1, start + k - 1);
+            while (start < end)
             {
 
+                char temp = ch[start];
+                ch[start] = ch[end];
+                ch[end] = temp;
+
+                start++;
+                end--;
             }
         }
-        return null;
+        return new String(ch);
     }
 
     public static void main(String[] args)
