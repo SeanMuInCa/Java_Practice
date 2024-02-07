94d25c727c6fdd70500bdba5310c8fe3201ee522
Sean <fengying1028@hotmail.com>
2024-02-07 11:19:35 -0600

    KMP

--- WinterBreak/src/Algrithm/String/AlgrithmInString.java
+++ WinterBreak/src/Algrithm/String/AlgrithmInString.java
@@ -117,6 +117,36 @@ public class AlgrithmInString
             end = start + 1;
         }
     }
+    //KMP
+    public static int strStr(String haystack, String needle) {
+        if (needle.isEmpty()) return 0;
+        int[] next = new int[needle.length()];
+        getNext(next, needle);
+
+        int j = 0;
+        for (int i = 0; i < haystack.length(); i++) {
+            while (j > 0 && needle.charAt(j) != haystack.charAt(i))
+                j = next[j - 1];
+            if (needle.charAt(j) == haystack.charAt(i))
+                j++;
+            if (j == needle.length())
+                return i - needle.length() + 1;
+        }
+        return -1;
+
+    }
+
+    private static void getNext(int[] next, String s) {
+        int j = 0;
+        next[0] = 0;
+        for (int i = 1; i < s.length(); i++) {
+            while (j > 0 && s.charAt(j) != s.charAt(i))
+                j = next[j - 1];
+            if (s.charAt(j) == s.charAt(i))
+                j++;
+            next[i] = j;
+        }
+    }
     public static void main(String[] args)
     {
         String str = "abcdefg";
