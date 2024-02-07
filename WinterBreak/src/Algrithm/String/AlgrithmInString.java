77c0a813a6b6f0e4ae9313634eb9048e2ee104ad
Sean <fengying1028@hotmail.com>
2024-02-06 20:58:02 -0600

    string method

--- WinterBreak/src/Algrithm/String/AlgrithmInString.java
+++ WinterBreak/src/Algrithm/String/AlgrithmInString.java
@@ -59,22 +59,68 @@ public class AlgrithmInString
         return new String(ch);
     }
 
-    public static String reverseWords(String str)
+    /**
+     * 不使用Java内置方法实现
+     * <p>
+     * 1.去除首尾以及中间多余空格
+     * 2.反转整个字符串
+     * 3.反转各个单词
+     */
+    public static String reverseWords(String s)
     {
-        StringBuilder sb = new StringBuilder(str.trim()).reverse();
-        StringBuilder res = new StringBuilder();
-        int left = 0;
-        int right = sb.length() - 1;
-        for (int i = 0; i < sb.length(); i++)
-        {
-            if(sb.charAt(i) == ' ' || i == right)
-            {
-                res.append(recReverse(sb.substring(left,i)));
-                left = i;
+        // System.out.println("ReverseWords.reverseWords2() called with: s = [" + s + "]");
+        // 1.去除首尾以及中间多余空格
+        StringBuilder sb = removeSpace(s);
+        // 2.反转整个字符串
+        reverseString(sb, 0, sb.length() - 1);
+        // 3.反转各个单词
+        reverseEachWord(sb);
+        return sb.toString();
+    }
+    private static StringBuilder removeSpace(String s) {
+        // System.out.println("ReverseWords.removeSpace() called with: s = [" + s + "]");
+        int start = 0;
+        int end = s.length() - 1;
+        while (s.charAt(start) == ' ') start++;
+        while (s.charAt(end) == ' ') end--;
+        StringBuilder sb = new StringBuilder();
+        while (start <= end) {
+            char c = s.charAt(start);
+            if (c != ' ' || sb.charAt(sb.length() - 1) != ' ') {
+                sb.append(c);
             }
+            start++;
         }
+        // System.out.println("ReverseWords.removeSpace returned: sb = [" + sb + "]");
+        return sb;
+    }
+    /**
+     * 反转字符串指定区间[start, end]的字符
+     */
+    public static void reverseString(StringBuilder sb, int start, int end) {
+        // System.out.println("ReverseWords.reverseString() called with: sb = [" + sb + "], start = [" + start + "], end = [" + end + "]");
+        while (start < end) {
+            char temp = sb.charAt(start);
+            sb.setCharAt(start, sb.charAt(end));
+            sb.setCharAt(end, temp);
+            start++;
+            end--;
+        }
+        // System.out.println("ReverseWords.reverseString returned: sb = [" + sb + "]");
+    }
 
-        return res.toString().trim();
+    private static void reverseEachWord(StringBuilder sb) {
+        int start = 0;
+        int end = 1;
+        int n = sb.length();
+        while (start < n) {
+            while (end < n && sb.charAt(end) != ' ') {
+                end++;
+            }
+            reverseString(sb, start, end - 1);
+            start = end + 1;
+            end = start + 1;
+        }
     }
     public static void main(String[] args)
     {
