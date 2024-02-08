87c459a866ba212a6d5cfc32ae0487208e7d1d48
Sean <fengying1028@hotmail.com>
2024-02-07 22:20:43 -0600

    valid brackets

--- WinterBreak/src/Algrithm/StackAndQueue/TestQueue.java
+++ WinterBreak/src/Algrithm/StackAndQueue/TestQueue.java
@@ -1,18 +1,38 @@
 package Algrithm.StackAndQueue;
 
+import java.util.Stack;
+
 /**
- * Purpose:             Algrithm.StackAndQueue<br />
- * Data Submitted:      2024/2/7 <br />
- * Assignment Number:    Algrithm.StackAndQueue<br />
- * Course Name:         COSC602  <br />
- * Instructor:          George Ding  <br />
- * File Path:          Algrithm.StackAndQueue <br />
- *
- * @author Zhenghua Mu
- * @version 1.0.0
+ Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+ determine if the input string is valid.
+
+ An input string is valid if:
+
+ Open brackets must be closed by the same type of brackets.
+ Open brackets must be closed in the correct order.
+ Every close bracket has a corresponding open bracket of the same type.
  */
 public class TestQueue
 {
+    public boolean isValid(String s)
+    {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++)
+        {
+            char c = s.charAt(i);
+            switch (c)
+            {
+                case '(' -> stack.push(')');
+                case '[' -> stack.push(']');
+                case '{' -> stack.push('}');
+                default -> {
+                    if(stack.isEmpty() || c != stack.pop())
+                        return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
     public static void main(String[] args)
     {
         MyQueue q = new MyQueue();
