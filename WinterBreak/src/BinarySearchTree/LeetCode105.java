57852471798ad642efd5a62f6ebc4daa288d98cf
Sean <fengying1028@hotmail.com>
2024-02-11 13:51:52 -0600

    105

---
+++ WinterBreak/src/BinarySearchTree/LeetCode105.java
@@ -0,0 +1,56 @@
+package BinarySearchTree;
+
+/**
+ * Purpose:             BinarySearchTree<br />
+ * Data Submitted:      2024/2/11 <br />
+ * Assignment Number:    BinarySearchTree<br />
+ * Course Name:         COSC602  <br />
+ * Instructor:          George Ding  <br />
+ * File Path:          BinarySearchTree <br />
+ *
+ * @author Zhenghua Mu
+ * @version 1.0.0
+ */
+public class LeetCode105
+{
+    /**
+     * Given two integer arrays preorder and inorder
+     * where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal
+     * of the same tree, construct and return the binary tree.
+     */
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return build(preorder,0,preorder.length - 1,
+                     inorder,0,inorder.length - 1);
+    }
+    TreeNode build(int[] preorder, int preStart, int preEnd,
+                   int[] inorder, int inStart, int inEnd) {
+
+        if (preStart > preEnd) {
+            return null;
+        }
+
+        // root 节点对应的值就是前序遍历数组的第一个元素
+        int rootVal = preorder[preStart];
+        // rootVal 在中序遍历数组中的索引
+        int index = 0;
+        for (int i = inStart; i <= inEnd; i++) {
+            if (inorder[i] == rootVal) {
+                index = i;
+                break;
+            }
+        }
+
+        int leftSize = index - inStart;
+
+        // 先构造出当前根节点
+        TreeNode root = new TreeNode(rootVal);
+        // 递归构造左右子树
+        root.left = build(preorder, preStart + 1, preStart + leftSize,
+                inorder, inStart, index - 1);
+
+        root.right = build(preorder, preStart + leftSize + 1, preEnd,
+                inorder, index + 1, inEnd);
+        return root;
+    }
+
+}
