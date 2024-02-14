fb22ba4c41ccbd4bf4f38fec4903688579bd2f57
Sean <fengying1028@hotmail.com>
2024-02-13 19:51:26 -0600

    sql lab2

--- WinterBreak/src/mysql/lab2.sql
+++ WinterBreak/src/mysql/lab2.sql
@@ -33,7 +33,62 @@ ON o.student = s.student
 GROUP BY s.student
 
 
-#4. The current table is not normalized and the structure limits what we #can do. For example, it would be difficult to create a report with all #the student information on one line. Begin the process of normalizing #this table by creating a student table that assigns ids to the students.
-#Then create a procedure that adds the students in the student_mark table #to the student table.
+#4. The current table is not normalized and the structure limits what we 
+#can do. For example, it would be difficult to create a report with all 
+#the student information on one line. Begin the process of normalizing 
+#this table by creating a student table that assigns ids to the students.
+#Then create a procedure that adds the students in the student_mark table 
+#to the student table.
 
+#create a table for students
+CREATE TABLE students(
+		student_id INT AUTO_INCREMENT,
+    student_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY(student_id)
+);
 
+ALTER TABLE student_mark ADD student_id INT;
+ALTER TABLE student_mark ADD FOREIGN KEY(student_id) REFERENCES students(student_id);
+
+DROP PROCEDURE IF EXISTS add_student;
+DELIMITER $
+CREATE PROCEDURE add_student()
+BEGIN
+ -- variable to hold student
+ DECLARE student_name VARCHAR(255); 
+
+ -- variable to track loop is done
+ DECLARE done INT DEFAULT FALSE;
+ 
+ -- create cursor
+ DECLARE student_cursor CURSOR FOR 
+	SELECT DISTINCT student FROM student_mark;
+ 
+ -- handler for cursor. tracks if records finished
+ DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+ 
+ -- open the cursor
+ OPEN student_cursor;
+ 
+ -- create loop
+ our_loop: LOOP
+ 
+	 -- add the cursor info into A VARIABLE
+	 FETCH student_cursor INTO student_name;
+     
+     -- true condition, records done
+     IF done THEN 
+		LEAVE our_loop;
+	ELSE 
+		-- add info to table
+		INSERT INTO students(student_name) VALUES (student_name); 
+        END IF;
+	-- end loop
+	END LOOP;
+    
+    -- close the cursor
+    CLOSE student_cursor;
+END $
+DELIMITER ;
+
+CALL add_student();
