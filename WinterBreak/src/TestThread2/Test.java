457eb886376a08ae6f9c0f977170247da9a7ee97
Sean <fengying1028@hotmail.com>
2024-02-15 10:36:58 -0600

    thread

---
+++ WinterBreak/src/TestThread2/Test.java
@@ -0,0 +1,33 @@
+package TestThread2;
+
+/**
+ * Purpose:             TestThread2<br />
+ * Data Submitted:      2024/2/15 <br />
+ * Assignment Number:    TestThread2<br />
+ * Course Name:         COSC602  <br />
+ * Instructor:          George Ding  <br />
+ * File Path:          TestThread2 <br />
+ *
+ * @author Zhenghua Mu
+ * @version 1.0.0
+ */
+public class Test
+{
+    public static void main(String[] args)
+    {
+        Runnable t1 = ()-> {
+            for (int i = 0; i < 10; i++)
+            {
+                System.out.println(Thread.currentThread().getName() + i);
+            }
+        };
+        Thread thread = new Thread(t1);
+        thread.start();
+        Thread thread1 = new Thread(t1);
+        thread1.start();
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.println(Thread.currentThread().getName() + i);
+        }
+    }
+}
