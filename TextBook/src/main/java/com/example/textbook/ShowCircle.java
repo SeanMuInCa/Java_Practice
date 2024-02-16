dc561887bc58fd8640d2dbbfef44ff017660a0f9
Sean <fengying1028@hotmail.com>
2024-02-15 21:55:30 -0600

    fx test

--- TextBook/src/main/java/com/example/textbook/ShowCircle.java
+++ TextBook/src/main/java/com/example/textbook/ShowCircle.java
@@ -1,5 +1,12 @@
 package com.example.textbook;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
 /**
  * Purpose:             com.example.textbook<br />
  * Data Submitted:      2024/2/15 <br />
@@ -11,7 +18,28 @@ package com.example.textbook;
  * @author Zhenghua Mu
  * @version 1.0.0
  */
-public class ShowCircle
+public class ShowCircle extends Application
 {
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        Pane pane = new Pane();
 
+        // Create a circle and set its properties
+        Circle circle = new Circle();
+        circle.centerXProperty().bind(pane.widthProperty().divide(2));
+        circle.centerYProperty().bind(pane.heightProperty().divide(2));
+        circle.setRadius(50);
+
+        circle.setStroke(Color.BLACK);
+        circle.setFill(Color.WHITE);
+        pane.getChildren().add(circle);
+
+        Scene scene = new Scene(pane);
+
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+
+        stage.show();
+    }
 }
