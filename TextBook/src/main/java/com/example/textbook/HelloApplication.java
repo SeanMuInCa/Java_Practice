f71aceac5783cb2c157b8c802ac24e21a9725116
Sean <fengying1028@hotmail.com>
2024-02-15 21:07:09 -0600

    fx test

--- TextBook/src/main/java/com/example/textbook/HelloApplication.java
+++ TextBook/src/main/java/com/example/textbook/HelloApplication.java
@@ -3,6 +3,11 @@ package com.example.textbook;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,11 +17,24 @@ public class HelloApplication extends Application
     @Override
     public void start(Stage stage) throws IOException
     {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 200);
+        Circle circle = new Circle();
+        circle.setCenterX(100);
+         circle.setCenterY(100);
+         circle.setRadius(50);
+         circle.setStroke(Color.BLACK);
+         circle.setFill(Color.WHITE);
+        StackPane pane = new StackPane(circle);
+        //pane.getChildren().add(new Button("OK"));
+        Scene scene = new Scene(pane, 500, 200);
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
+        /*Stage newStage = new Stage();
+        newStage.setTitle("new");
+        newStage.setScene(new Scene(new Button("new OK"),200,200));
+        newStage.show();*/
     }
 
     public static void main(String[] args)
---
+++ TextBook/src/main/java/com/example/textbook/ShowCircle.java
@@ -0,0 +1,17 @@
+package com.example.textbook;
+
+/**
+ * Purpose:             com.example.textbook<br />
+ * Data Submitted:      2024/2/15 <br />
+ * Assignment Number:    com.example.textbook<br />
+ * Course Name:         COSC602  <br />
+ * Instructor:          George Ding  <br />
+ * File Path:          com.example.textbook <br />
+ *
+ * @author Zhenghua Mu
+ * @version 1.0.0
+ */
+public class ShowCircle
+{
+
+}
