016b2404bf58b8c59a30a82f4ab7565bd049c933
Sean <fengying1028@hotmail.com>
2024-02-18 17:58:42 -0600

    thread

--- WinterBreak/src/TestThread3/Tickets.java
+++ WinterBreak/src/TestThread3/Tickets.java
@@ -14,18 +14,25 @@ package TestThread3;
 public class Tickets
 {
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws InterruptedException
     {
         SellTickets1 s1 = new SellTickets1();
         //SellTickets1 s2 = new SellTickets1();
         //SellTickets1 s3 = new SellTickets1();
-        new Thread(s1).start();
-        new Thread(s1).start();
-        new Thread(s1).start();
+        Thread thread = new Thread(s1);
+
+        Thread thread1 = new Thread(s1);
+
+        Thread thread2 = new Thread(s1);
+        thread.start();
+        thread1.start();
+        thread2.start();
+
         //都会出现问题，不管是继承方式，还是接口方式
         //必须要加锁才能解决这个问题
     }
 }
+
 class SellTickets extends Thread
 {
     private static int num = 10;
@@ -33,38 +40,57 @@ class SellTickets extends Thread
     @Override
     public void run()
     {
-        while (num > 0)
+        while (true)
         {
-            System.out.println("Counter " + Thread.currentThread().getName() + " " + (num--));
-            try
-            {
-                Thread.sleep(100);
-            } catch (InterruptedException e)
-            {
-                throw new RuntimeException(e);
-            }
+            buy();
+        }
+    }
+
+    public synchronized void buy()
+    {
+        if (num < 0) return;
+        System.out.println("Counter " + Thread.currentThread().getName() + " " + (num));
+        num--;
+        try
+        {
+            Thread.sleep(1000);
+        } catch (InterruptedException e)
+        {
+            throw new RuntimeException(e);
         }
     }
 }
+
 class SellTickets1 implements Runnable
 {
-    private int num = 10;
+    private int num = 100;
+    boolean flag = true;
+
     @Override
-    public  void run()
+    public void run()
     {
-        synchronized (this)
+        while (flag)
         {
-            while (num > 0)
-            {
-                System.out.println("Counter " + Thread.currentThread().getName() + " " + (num--));
-                try
-                {
-                    Thread.sleep(50);
-                } catch (InterruptedException e)
-                {
-                    throw new RuntimeException(e);
-                }
-            }
+            buy();
+        }
+
+    }
+
+    public synchronized void buy()
+    {
+        if (num <= 0)
+        {
+            flag = false;
+            return;
+        }
+        System.out.println("Counter " + Thread.currentThread().getName() + " " + (num--));
+        try
+        {
+            Thread.sleep(50);
+            //Thread.yield();
+        } catch (InterruptedException e)
+        {
+            throw new RuntimeException(e);
         }
     }
 }
