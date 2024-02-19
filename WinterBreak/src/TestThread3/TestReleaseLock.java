c780295a4892249ad93416f26f6889a6aeddf057
Sean <fengying1028@hotmail.com>
2024-02-19 10:48:49 -0600

    thread

---
+++ WinterBreak/src/TestThread3/TestReleaseLock.java
@@ -0,0 +1,29 @@
+package TestThread3;
+
+/**
+ * Purpose:             TestThread3<br />
+ * Data Submitted:      2024/2/19 <br />
+ * Assignment Number:    TestThread3<br />
+ * Course Name:         COSC602  <br />
+ * Instructor:          George Ding  <br />
+ * File Path:          TestThread3 <br />
+ *
+ * @author Zhenghua Mu
+ * @version 1.0.0
+ */
+public class TestReleaseLock
+{
+    /**
+     * 释放锁
+     * 1. 执行完
+     * 2. 遇到break或者return
+     * 3. 出现异常了
+     * 4. 遇到了wait()方法
+     */
+    /**
+     * 不释放锁
+     * 1. sleep方法
+     * 2. yield方法
+     * 3. suspend/resume方法//不应当使用的方法
+     */
+}
