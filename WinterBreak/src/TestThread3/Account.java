f7504b4cf1e094d2ef00e2815de34b942ecff696
Sean <fengying1028@hotmail.com>
2024-02-19 17:22:08 -0600

    thread

--- WinterBreak/src/TestThread3/Account.java
+++ WinterBreak/src/TestThread3/Account.java
@@ -11,6 +11,45 @@ package TestThread3;
  * @author Zhenghua Mu
  * @version 1.0.0
  */
-public class Account
+public class Account implements Runnable
 {
+    static String accountName = "myAccount";
+    int number = 10000;
+    boolean flag = true;
+
+    public synchronized void withdraw(int amount) throws InterruptedException
+    {
+        if (number <= 0)
+        {
+            flag = false;
+            return;
+        }
+        number -= 1000;
+        Thread.sleep(500);
+        System.out.println(Thread.currentThread().getName() + " " + number);
+
+    }
+
+    @Override
+    public void run()
+    {
+        try
+        {
+            while (flag)
+            {
+                withdraw(1000);
+            }
+        } catch (InterruptedException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Account t1 = new Account();
+        new Thread(t1).start();
+        new Thread(t1).start();
+        new Thread(t1).start();
+    }
 }
