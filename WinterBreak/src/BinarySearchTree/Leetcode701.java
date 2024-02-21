321d41a3482bf65ddef2db8a342cf2f29b5dfc06
Sean <fengying1028@hotmail.com>
2024-02-21 14:42:25 -0600

    bst

---
+++ WinterBreak/src/BinarySearchTree/Leetcode701.java
@@ -0,0 +1,27 @@
+package BinarySearchTree;
+
+/**
+ * Purpose:             BinarySearchTree<br />
+ * Data Submitted:      2024/2/21 <br />
+ * Assignment Number:    BinarySearchTree<br />
+ * Course Name:         COSC602  <br />
+ * Instructor:          George Ding  <br />
+ * File Path:          BinarySearchTree <br />
+ *
+ * @author Zhenghua Mu
+ * @version 1.0.0
+ */
+public class Leetcode701
+{
+    TreeNode<Integer> insertIntoBST(TreeNode<Integer> root, int val) {
+        // 找到空位置插入新节点
+        if (root == null) return new TreeNode(val);
+        // if (root.val == val)
+        //     BST 中一般不会插入已存在元素
+        if (root.val < val)
+            root.right = insertIntoBST(root.right, val);
+        if (root.val > val)
+            root.left = insertIntoBST(root.left, val);
+        return root;
+    }
+}
