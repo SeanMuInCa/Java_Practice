ebbb320d36a28160ff406423ca313f086801047c
Sean <fengying1028@hotmail.com>
2024-02-24 12:06:28 -0600

    bst

--- WinterBreak/src/BinarySearchTree/BST.java
+++ WinterBreak/src/BinarySearchTree/BST.java
@@ -305,6 +305,37 @@ public class BST<E> implements Tree<E>
         // 递归比较左子树和右子树
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
+    public boolean isSymmetric1(TreeNode root) {
+        return compare(root.left, root.right);
+    }
+
+    /**
+     * 比较是否是对称二叉树
+     * @param left
+     * @param right
+     * @return
+     */
+    private boolean compare(TreeNode left, TreeNode right) {
+
+        if (left == null && right != null) {
+            return false;
+        }
+        if (left != null && right == null) {
+            return false;
+        }
+
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left.data != right.data) {
+            return false;
+        }
+        // 比较外侧
+        boolean compareOutside = compare(left.left, right.right);
+        // 比较内侧
+        boolean compareInside = compare(left.right, right.left);
+        return compareOutside && compareInside;
+    }
 
     @Override
     public boolean delete(E e)
