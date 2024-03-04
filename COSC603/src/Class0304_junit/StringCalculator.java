b51fb12a46131bde4230c27d81c95e46f31465d0
Sean <fengying1028@hotmail.com>
2024-03-04 15:51:37 -0600

    junit

---
+++ COSC603/src/Class0304_junit/StringCalculator.java
@@ -0,0 +1,20 @@
+package Class0304_junit;
+
+/**
+ * Purpose:             Class0304_junit<br />
+ * Data Submitted:      2024/3/4 <br />
+ * Assignment Number:    Class0304_junit<br />
+ * Course Name:         COSC602  <br />
+ * Instructor:          George Ding  <br />
+ * File Path:          Class0304_junit <br />
+ *
+ * @author Zhenghua Mu
+ * @version 1.0.0
+ */
+public class StringCalculator
+{
+    public static int add(String numbers)
+    {
+        return 0;
+    }
+}
---
+++ COSC603/test/Class0304_junit/StringCalculatorTest.java
@@ -0,0 +1,26 @@
+package Class0304_junit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Purpose:             Class0304_junit<br />
+ * Data Submitted:      2024/3/4 <br />
+ * Assignment Number:    Class0304_junit<br />
+ * Course Name:         COSC602  <br />
+ * Instructor:          George Ding  <br />
+ * File Path:          Class0304_junit <br />
+ *
+ * @author Zhenghua Mu
+ * @version 1.0.0
+ */
+class StringCalculatorTest
+{
+
+    @Test
+    void add()
+    {
+
+    }
+}
