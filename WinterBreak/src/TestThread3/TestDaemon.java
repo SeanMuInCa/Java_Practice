a7d72bd4cd52b9de26b83725c404d7bb8fc7d5c5
Sean <fengying1028@hotmail.com>
2024-03-10 19:13:02 -0600

    thread

--- WinterBreak/src/TestThread3/TestDaemon.java
+++ WinterBreak/src/TestThread3/TestDaemon.java
@@ -28,11 +28,28 @@ public class TestDaemon
                 System.out.println("hello");
             }
         };
+        Runnable t2 = ()->{
+            int n = 10;
+            while (n-- > 0)
+            {
+                try
+                {
+                    Thread.sleep(50);
+                } catch (InterruptedException e)
+                {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("hi");
+            }
+        };
         Thread thread = new Thread(t1);
+        Thread thread1 = new Thread(t2);
         thread.setDaemon(true);//设置为守护线程，这样当主线程结束后，子线程也结束
+        thread1.setDaemon(true);//设置为守护线程，这样当主线程结束后，子线程也结束
         thread.start();
+        thread1.start();
 
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 50; i++)
         {
             System.out.println(i);
             Thread.sleep(50);
