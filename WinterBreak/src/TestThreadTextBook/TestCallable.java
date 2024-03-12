078c24870cd99be9d5e640ba78230572eb6d07d2
Sean <fengying1028@hotmail.com>
2024-03-12 11:38:49 -0600

    thread

--- WinterBreak/src/TestThreadTextBook/TestCallable.java
+++ WinterBreak/src/TestThreadTextBook/TestCallable.java
@@ -1,5 +1,9 @@
 package TestThreadTextBook;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
 /**
  * Purpose:             TestThreadTextBook<br />
  * Data Submitted:      2024/3/12 <br />
@@ -13,9 +17,30 @@ package TestThreadTextBook;
  */
 public class TestCallable
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws ExecutionException, InterruptedException
     {
         //callable有返回值，可以抛出异常，还是一个泛型方法
         //相比较runable没有返回值也不能抛出异常
+        //用funtureTask来接收返回值
+        MyCallable myCallable = new MyCallable();
+        FutureTask<Integer> integerFutureTask = new FutureTask<Integer>(myCallable);//这个也实现了runable接口
+
+        new Thread(integerFutureTask).start();
+        System.out.println(integerFutureTask.get());
+    }
+}
+class MyCallable implements Callable<Integer>
+{
+
+    @Override
+    public Integer call() throws Exception
+    {
+        int sum = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.println(Thread.currentThread().getName() + " " + i);
+            sum += i;
+        }
+        return sum;
     }
 }
