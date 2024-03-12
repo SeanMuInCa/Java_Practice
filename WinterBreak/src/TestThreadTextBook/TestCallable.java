a131900a27b4557c10d32b7b92981ad6d6df8e67
Sean <fengying1028@hotmail.com>
2024-03-12 11:29:49 -0600

    thread

---
+++ WinterBreak/src/TestThreadTextBook/TestCallable.java
@@ -0,0 +1,21 @@
+package TestThreadTextBook;
+
+/**
+ * Purpose:             TestThreadTextBook<br />
+ * Data Submitted:      2024/3/12 <br />
+ * Assignment Number:    TestThreadTextBook<br />
+ * Course Name:         COSC603  <br />
+ * Instructor:          Michael Grzesina  <br />
+ * File Path:          TestThreadTextBook <br />
+ *
+ * @author Zhenghua Mu
+ * @version 1.0.0
+ */
+public class TestCallable
+{
+    public static void main(String[] args)
+    {
+        //callable有返回值，可以抛出异常，还是一个泛型方法
+        //相比较runable没有返回值也不能抛出异常
+    }
+}
